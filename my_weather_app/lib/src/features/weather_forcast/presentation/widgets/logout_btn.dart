5991691c95e14984629dca82633aab35cd48b4f6
devkrishnan <devkrishnan.va@innovaturelabs.com>
2024-05-30 18:18:51 +0530

    feat: logout dialog

--- my_weather_app/lib/src/features/weather_forcast/presentation/widgets/logout_btn.dart
+++ my_weather_app/lib/src/features/weather_forcast/presentation/widgets/logout_btn.dart
@@ -19,10 +19,44 @@ class LogoutBtn extends StatelessWidget {
         color: Colors.white,
       ),
       onPressed: () async {
-        await Authentication(FirebaseAuth.instance).logOut();
-        context.mounted
-            ? AutoRouter.of(context).replaceAll([const LoginRoute()])
-            : debugPrint("context wasn't mounted");
+        showLogoutConfirmationDialog(context);
+      },
+    );
+  }
+
+  Future<void> showLogoutConfirmationDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // User must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: AppColors.primaryColor,
+          title: const Text('Logout'),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Are you sure you want to log out?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+            TextButton(
+              child: const Text('Logout'),
+              onPressed: () async {
+                await Authentication(FirebaseAuth.instance).logOut();
+                context.mounted
+                    ? AutoRouter.of(context).replaceAll([const LoginRoute()])
+                    : debugPrint("context wasn't mounted");
+              },
+            ),
+          ],
+        );
       },
     );
   }
