9c170742b7b75a36ca92468588d49699642db61e
devkrishnan <devkrishnan.va@innovaturelabs.com>
2024-06-03 10:04:07 +0530

    feat: enforced api list call once a day

--- my_weather_app/lib/src/core/init_dependencies.dart
+++ my_weather_app/lib/src/core/init_dependencies.dart
@@ -3,34 +3,44 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:my_weather_app/firebase_options.dart';
 import 'package:my_weather_app/src/core/config/config.dart';
+import 'package:my_weather_app/src/core/services/date_formatter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class InitDependencies {
   final Dio dio;
   InitDependencies(this.dio);
   static Future<void> initFirebase() async {
+    // function to initialize firebase
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
   }
 
-  static void setDeviceOrientation() async {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
-  }
+  // static void setDeviceOrientation() async {
+
+  //   SystemChrome.setPreferredOrientations([
+  //     DeviceOrientation.portraitUp,
+  //   ]);
+  // }
 
   Future<dynamic> initLocationList() async {
+    // function to fetch and update location list
     try {
       SharedPreferences sharedPreferences =
           await SharedPreferences.getInstance();
-      Response res = await dio.get(locationListURL);
-      sharedPreferences.setString("location_list", jsonEncode(res.data));
-      debugPrint("fetched location list");
-      return jsonEncode(res.data);
+      int? previouseFetchedTime =
+          sharedPreferences.getInt("location_list_updated_date");
+      if (previouseFetchedTime == null ||
+          DateFormatter.isPastOneDay(previouseFetchedTime)) {
+        Response res = await dio.get(locationListURL);
+        sharedPreferences.setString("location_list", jsonEncode(res.data));
+        sharedPreferences.setInt("location_list_updated_date",
+            DateTime.now().millisecondsSinceEpoch);
+        debugPrint("Fetched location list");
+        return jsonEncode(res.data);
+      }
     } catch (e) {
       debugPrint(
           "WARNING !! : Couldn't fetch location list device maybe offline");
--- my_weather_app/lib/src/core/services/date_formatter.dart
+++ my_weather_app/lib/src/core/services/date_formatter.dart
@@ -6,11 +6,19 @@ class DateFormatter {
       DateFormat('dd-MM-yy HH:mm').format(dateTime);
 
   static String getDayasText(DateTime dateTime) {
+    // function get day's as text such as monday , tuesday etc
     final dayFormatter = DateFormat('EEEE'); // EEEE for full weekday name
     String dayName = dayFormatter.format(dateTime);
     return dayName;
   }
 
   static String formatTime(DateTime dateTime) =>
+      // function to get time in AM/PM format
       DateFormat('hh:mm a').format(dateTime);
+
+  static bool isPastOneDay(int previousTime) =>
+      // function to check weather given time has passed one day
+      DateTime.now().millisecondsSinceEpoch - previousTime > 86400000
+          ? true
+          : false;
 }
